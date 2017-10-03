4db08d2defd5c0947ad134acb09e0bc68fe7c865
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-10-03 16:46:30 +0100

    bootstrap config & routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PersonalComponent } from './form/personal/personal.component';
+import { WorkComponent } from './form/work/work.component';
+import { AddressComponent } from './form/address/address.component';
+import { ResultComponent } from './form/result/result.component';
+
+import { FormDataService } from './form-data.service';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'personal', component: PersonalComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'address', component: AddressComponent },
+  { path: 'result', component: ResultComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonalComponent,
+    WorkComponent,
+    AddressComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [FormDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

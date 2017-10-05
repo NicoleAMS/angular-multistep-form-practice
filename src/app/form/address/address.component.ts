a8c7715668235df1908d06715436fe7ddfbbd6e3
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-10-05 13:23:43 +0100

    address feature added

--- src/app/form/address/address.component.ts
+++ src/app/form/address/address.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Address } from '../data/form-data.model';
+import { FormDataService } from '../data/form-data.service';
 
 @Component({
   selector: 'app-address',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
+  title = 'Where do you live?';
+  address: Address;
+  form: any;
 
-  constructor() { }
+  constructor(private router: Router, private formDataService: FormDataService) { }
 
   ngOnInit() {
+    this.address = this.formDataService.getAddress();
+    console.log('Address feature loaded!');
+  }
+
+  save(form: any): boolean {
+    if (!form.valid) {
+      return false;
+    }
+    this.formDataService.setAddress(this.address);
+    return true;
+  }
+
+  goToPrevious(form: any) {
+    if (this.save(form)) {
+      this.router.navigate(['/work']);
+    }
+  }
+
+  goToNext(form: any) {
+    if (this.save(form)) {
+      this.router.navigate(['/result']);
+    }
   }
 
 }

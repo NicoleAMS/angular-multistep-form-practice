cae8aa832657be1ee0194928d1f287567188b246
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-10-05 12:59:33 +0100

    personal feature added

--- src/app/form/personal/personal.component.ts
+++ src/app/form/personal/personal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Personal } from '../data/form-data.model';
+import { FormDataService } from '../data/form-data.service';
 
 @Component({
   selector: 'app-personal',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal.component.css']
 })
 export class PersonalComponent implements OnInit {
+  title = 'Please tell us about yourself.';
+  personal: Personal;
+  form: any;
 
-  constructor() { }
+  constructor(private router: Router, private formDataService: FormDataService) { }
 
   ngOnInit() {
+    this.personal = this.formDataService.getPersonal();
+    console.log('Personal feature loaded!');
+  }
+
+  save(form: any): boolean {
+    if (!form.valid) {
+      return false;
+    }
+    this.formDataService.setPersonal(this.personal);
+    console.log(this.personal);
+    return true;
+  }
+
+  goToNext(form: any) {
+    if (this.save(form)) {
+      this.router.navigate(['/work']);
+    }
   }
 
 }

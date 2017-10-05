0b3c2d2e14d59deb4bf6540d19b7d7a656cb3a50
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-10-05 13:09:20 +0100

    work feature added

--- src/app/form/work/work.component.ts
+++ src/app/form/work/work.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormDataService } from '../data/form-data.service';
+
 
 @Component({
   selector: 'app-work',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
+  title = 'What do you do?';
+  workType: string;
+  form: any;
 
-  constructor() { }
+  constructor(private router: Router, private formDataService: FormDataService) { }
 
   ngOnInit() {
+    this.workType = this.formDataService.getWork();
+    console.log('Work feature loaded!');
+  }
+
+  save(form: any) {
+    if (!form.valid) {
+      return false;
+    }
+    this.formDataService.setWork(this.workType);
+    return true;
+  }
+
+  goToPrevious(form: any) {
+    if (this.save(form)) {
+      this.router.navigate(['/personal']);
+    }
+  }
+
+  goToNext(form: any) {
+    if (this.save(form)) {
+      this.router.navigate(['/address']);
+    }
   }
 
 }
